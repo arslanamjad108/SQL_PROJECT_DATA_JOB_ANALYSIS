7c6e0e46ab76ee5f3a7cdc16c7e3b3e83d4ab2cb
Muhammad Arslan Amjad Qureshi <u7632488@anu.edu.au>
2026-05-14 18:35:13 +1000

    Added the comments to solve the top paying skills

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -8,6 +8,13 @@
   and helps identify the most financially rewarding skills to acquire or improve.
 */
 
+-- I solved this query problem step-wise, and this is what I followed
+/*
+1. Find the Data Analyst job postings
+2. Connects them to required skills
+3. Calculate the average salary for each skill
+4. Sort skills by highest average salary
+*/
 -- Calculates the average salary for job postings by individual skill 
 SELECT
   skills_dim.skills AS skill, 
@@ -28,4 +35,3 @@ ORDER BY
   avg_salary DESC;
 
 -- Top Paying skills for the Data Analyst Role
-
